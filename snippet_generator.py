dd68133c2294e1b113c82e6606276c56e3fc8a78
Srikanth Chelluri <srikanth.chelluri@appian.com>
2016-06-20 17:11:07 -0400

    initial commit, code dump

---
+++ snippet_generator.py
@@ -0,0 +1,94 @@
+# Srikanth Chelluri
+# June 2016
+# Appian SAIL internal functions ST3 snippet generator
+# Note: use completions for generic functions 
+
+from os import listdir
+from os.path import isfile, join
+import os
+import getpass
+
+# Automated for Mac or PC
+user = getpass.getuser()
+path_sysrules = ""
+if os.name == "nt":
+	path_sysrules = "C:/Users/" + user + "/repo/ae/bundled-apps/system/content/"
+elif os.name == "posix":
+	path_sysrules = "/Users/" + user + "/repo/ae/bundled-apps/system/content/"
+else:
+	print("Not compatible with OS. Exiting.")
+	exit()
+
+try:
+	dirs = [f for f in listdir(path_sysrules) if isfile(join(path_sysrules, f))]
+except:
+	print("Can't find directory.")
+	exit()
+
+for file in dirs:
+	# Don't look at anything other than rules themselves
+	if "SYSTEM_CONTENT" in str(file) or "SYSTEM_FOLDER" in str(file):
+		continue
+	# These files have comments and seem to be somewhat 'beta' - not parsing them
+	if str(file) == "SYSTEM_SYSRULES_appdesigner_impactAnalysis_modal_controller.xml" or str(file) == "SYSTEM_SYSRULES_appdesigner_impactAnalysis_modal_view.xml":
+		continue
+
+	function = str(file[16:][:-4])
+	if function == "":
+		continue
+
+	if function[0:6] != 'FOLDER':
+		input_file = open(join(path_sysrules, file), 'r')
+		prev_line = ""
+		check_line = ""
+		arg_list = [] # List of arguments for function
+		type_list = [] # List of argument types for function (one-to-one mapping)
+		for line in input_file:
+			prev_line = check_line
+			check_line = line.strip()
+			if prev_line == "<namedTypedValue>":
+				arg_list.append(check_line[6:][:-7])
+			if prev_line == "<type>":
+				type_list.append(check_line[6:][:-7])
+
+		output_file = ""
+		if os.name == "posix":
+			if not os.path.exists("/Users/" + user + "/Library/Application Support/Sublime Text 3/Packages/User/sail-snippets/"):
+				os.makedirs("/Users/" + user + "/Library/Application Support/Sublime Text 3/Packages/User/sail-snippets/")
+			output_file = open("/Users/" + user + "/Library/Application Support/Sublime Text 3/Packages/User/sail-snippets/" + function + ".sublime-snippet", 'w')
+		else:
+			if not os.path.exists("C:/Users/" + user + "/AppData/Roaming/Sublime Text 3/Packages/User/sail-snippets/"):
+				os.makedirs("C:/Users/" + user + "/AppData/Roaming/Sublime Text 3/Packages/User/sail-snippets/")
+			output_file = open("C:/Users/" + user + "/AppData/Roaming/Sublime Text 3/Packages/User/sail-snippets/" + function + ".sublime-snippet", 'w')
+		
+		arg_count = 0
+
+		snippet = ""
+		# Optional: add "a!" domain (but if user enters the domain, it may repeat, so it's left out for now - personal preference)
+		if len(arg_list) == 0:
+			snippet = "" + function + "()"
+		elif len(arg_list) == 1:
+			# One line (for clarity), highlight target AND value (less verbose)
+			snippet = "" + function + "(${1:" + arg_list[0] + ": " + type_list[0] + "})"
+		elif len(arg_list) == 2:
+			# One line (for clarity), highlight only values (for clarity)
+			snippet = "" + function + "(" + arg_list[0] + ": ${1:" + type_list[0] + "}, " + arg_list[1] + ": ${2:" + type_list[1] + "})"
+		else:
+			# Multiple lines (for clarity), Highlight only values (for clarity)
+			snippet = "" + function + "(\n"
+			for arg in arg_list:
+				if arg_count != len(arg_list) - 1:
+					snippet += "\t" + arg + ": ${" + str(arg_count + 1) + ":" + type_list[arg_count] + "},\n"
+				else:
+					snippet += "\t" + arg + ": ${" + str(arg_count + 1) + ":" + type_list[arg_count] + "}\n"
+				arg_count += 1
+			snippet += ")"
+		tab_trigger = function
+		description = "Appian Internal"
+
+		output_file.write("<snippet>\n")
+		output_file.write("\t<content><![CDATA[" + snippet + "]]></content>\n")
+		output_file.write("\t<tabTrigger>" + tab_trigger + "</tabTrigger>\n")
+		output_file.write("\t<scope>source.sail</scope>\n")
+		output_file.write("\t<description>" + description + "</description>\n")
+		output_file.write("</snippet>\n")

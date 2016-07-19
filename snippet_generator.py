98b86e98a6a074b8ab5c55d4be05fe1e5cfc44da
Srikanth Chelluri <srikanth.chelluri@appian.com>
2016-07-19 08:27:32 -0400

    Cleaned up

--- snippet_generator.py
+++ snippet_generator.py
@@ -1,12 +1,14 @@
 # Srikanth Chelluri
 # June 2016
-# Appian SAIL internal functions ST3 snippet generator
+# Appian SAIL internal functions Sublime Text 3 snippet generator
 # Note: use completions for generic functions 
 
+import os
 from os import listdir
 from os.path import isfile, join
-import os
 import getpass
+
+print "Running Sublime Text 3 snippet generation for Appian SAIL..."
 
 # Automated for Mac or PC
 user = getpass.getuser()
@@ -22,14 +24,16 @@ else:
 try:
 	dirs = [f for f in listdir(path_sysrules) if isfile(join(path_sysrules, f))]
 except:
-	print("Can't find directory. In Windows, 'repo' should be in C:/. In Mac OS X, 'repo' should be in /Users/[username]/.")
+	print("Can't find directory. In Windows, 'repo' should be in C:/. In Mac OS X, 'repo' should be in /Users/" + user + "/. Exiting.")
 	exit()
 
+snippet_count = 0
+
 for file in dirs:
-	# Don't look at anything other than rules themselves
+	# Omit anything that's not clearly a rule
 	if "SYSTEM_CONTENT" in str(file) or "SYSTEM_FOLDER" in str(file):
 		continue
-	# These files have comments and seem to be somewhat 'beta' - not parsing them
+	# These rules have comments in their parameters
 	if str(file) == "SYSTEM_SYSRULES_appdesigner_impactAnalysis_modal_controller.xml" or str(file) == "SYSTEM_SYSRULES_appdesigner_impactAnalysis_modal_view.xml":
 		continue
 
@@ -66,16 +70,16 @@ for file in dirs:
 		snippet = ""
 		# Optional: add "a!" domain (but if user enters the domain, it may repeat, so it's left out for now - personal preference)
 		if len(arg_list) == 0:
-			snippet = "" + function + "()"
+			snippet = function + "()" # With domain, snippet = "a!" + function + "()"
 		elif len(arg_list) == 1:
 			# One line (for clarity), highlight target AND value (less verbose)
-			snippet = "" + function + "(${1:" + arg_list[0] + ": " + type_list[0] + "})"
+			snippet = function + "(${1:" + arg_list[0] + ": " + type_list[0] + "})"
 		elif len(arg_list) == 2:
 			# One line (for clarity), highlight only values (for clarity)
-			snippet = "" + function + "(" + arg_list[0] + ": ${1:" + type_list[0] + "}, " + arg_list[1] + ": ${2:" + type_list[1] + "})"
+			snippet = function + "(" + arg_list[0] + ": ${1:" + type_list[0] + "}, " + arg_list[1] + ": ${2:" + type_list[1] + "})"
 		else:
-			# Multiple lines (for clarity), Highlight only values (for clarity)
-			snippet = "" + function + "(\n"
+			# Multiple lines (for clarity), highlight only values (for clarity)
+			snippet = function + "(\n"
 			for arg in arg_list:
 				if arg_count != len(arg_list) - 1:
 					snippet += "\t" + arg + ": ${" + str(arg_count + 1) + ":" + type_list[arg_count] + "},\n"
@@ -86,9 +90,12 @@ for file in dirs:
 		tab_trigger = function
 		description = "Appian Internal"
 
+		snippet_count += 1
 		output_file.write("<snippet>\n")
 		output_file.write("\t<content><![CDATA[" + snippet + "]]></content>\n")
 		output_file.write("\t<tabTrigger>" + tab_trigger + "</tabTrigger>\n")
 		output_file.write("\t<scope>source.sail</scope>\n")
 		output_file.write("\t<description>" + description + "</description>\n")
 		output_file.write("</snippet>\n")
+
+print str(snippet_count) + " rules processed. Exiting."

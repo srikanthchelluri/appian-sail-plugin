134dbacf502e7c2a9e13574a1a415bbc5c1eac58
Srikanth Chelluri <srikanth.chelluri@appian.com>
2016-07-30 17:03:49 -0400

    Add (almost) all directories

--- snippet_generator.py
+++ snippet_generator.py
@@ -13,36 +13,57 @@ print "Running Sublime Text 3 snippet generation (a!) for Appian SAIL..."
 # Automated for Mac or PC
 user = getpass.getuser()
 path_sysrules = ""
+path_sdxsail = ""
+path_erdsail = ""
+path_idsail = ""
+path_sitesdesignersail = ""
 if os.name == "nt":
 	path_sysrules = "C:/repo/ae/bundled-apps/system/content/"
+	path_sdxsail = "C:/repo/ae/assembled-bundled-apps/sdx-sail/content/"
+	path_erdsail = "C:/repo/ae/assembled-bundled-apps/erd-sail/content/"
+	path_idsail = "C:/repo/ae/assembled-bundled-apps/interface-designer-sail/content/"
+	path_sitesdesignersail = "C:/repo/ae/assembled-bundled-apps/sites-designer-sail/content/"
 elif os.name == "posix":
 	path_sysrules = "/Users/" + user + "/repo/ae/bundled-apps/system/content/"
+	path_sdxsail = "/Users/" + user + "/repo/ae/assembled-bundled-apps/sdx-sail/content/"
+	path_erdsail = "/Users/" + user + "/repo/ae/assembled-bundled-apps/erd-sail/content/"
+	path_idsail = "/Users/" + user + "/repo/ae/assembled-bundled-apps/interface-designer-sail/content/"
+	path_sitesdesignersail = "/Users/" + user + "/repo/ae/assembled-bundled-apps/sites-designer-sail/content/"
 else:
 	print("Not compatible with OS. Exiting.")
 	exit()
 
 try:
-	dirs = [f for f in listdir(path_sysrules) if isfile(join(path_sysrules, f))]
+	sysrule_files = [join(path_sysrules, f) for f in listdir(path_sysrules) if isfile(join(path_sysrules, f))]
+	sysrule_files.extend([join(path_sdxsail, f) for f in listdir(path_sdxsail) if isfile(join(path_sdxsail, f))])
+	sysrule_files.extend([join(path_erdsail, f) for f in listdir(path_erdsail) if isfile(join(path_erdsail, f))])
+	sysrule_files.extend([join(path_idsail, f) for f in listdir(path_idsail) if isfile(join(path_idsail, f))])
+	sysrule_files.extend([join(path_sitesdesignersail, f) for f in listdir(path_sitesdesignersail) if isfile(join(path_sitesdesignersail, f))])
+	print "Directories to be processed: "
+	print "\t" + path_sysrules + "\n\t" + path_sdxsail + "\n\t" + path_erdsail + "\n\t" + path_idsail + "\n\t" + path_sitesdesignersail
 except:
 	print("Can't find directory. In Windows, 'repo' should be in C:/. In Mac OS X, 'repo' should be in /Users/" + user + "/. Exiting.")
 	exit()
 
 snippet_count = 0
 
-for file in dirs:
+for file in sysrule_files:
+	file_name = file[file.rfind("/") + 1:] # file is entire path, file_name is just file itself
+
 	# Omit anything that's not clearly a rule
-	if "SYSTEM_CONTENT" in str(file) or "SYSTEM_FOLDER" in str(file):
+	if "SYSTEM_CONTENT" in str(file_name) or "SYSTEM_FOLDER" in str(file_name):
 		continue
 	# These rules have comments in their parameters
-	if str(file) == "SYSTEM_SYSRULES_appdesigner_impactAnalysis_modal_controller.xml" or str(file) == "SYSTEM_SYSRULES_appdesigner_impactAnalysis_modal_view.xml":
+	if str(file_name) == "SYSTEM_SYSRULES_appdesigner_impactAnalysis_modal_controller.xml" or str(file_name) == "SYSTEM_SYSRULES_appdesigner_impactAnalysis_modal_view.xml":
 		continue
 
-	function = str(file[16:][:-4])
+	function = str(file_name[16:][:-4])
 	if function == "":
 		continue
 
 	if function[0:6] != 'FOLDER':
-		input_file = open(join(path_sysrules, file), 'r')
+		# input_file = open(join(path_sysrules, file), 'r')
+		input_file = open(file, 'r')
 		prev_line = ""
 		check_line = ""
 		arg_list = [] # List of arguments for function
